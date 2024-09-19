74124d65b846b27784dde9f8f7b0503bb908fb52
melia <melia.reriouedj@shenron.eu>
2024-09-19 18:38:18 +0200

    Add profil

---
+++ components/Profil.js
@@ -0,0 +1,12 @@
+import styles from "../styles/Home.module.css";
+import TextFieldComponent from "./TexfieldComponent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+
+function Profil(){
+
+
+}
+
+export default Profil;
